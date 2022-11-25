58168676a4b467a4be04835051ff8f9f1835b02d
shajn786 <shajn786@gmail.com>
2022-11-25 15:29:25 +0530

    viewall

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddPostComponent } from './add-post/add-post.component';
+import { ViewAllPostComponent } from './view-all-post/view-all-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddPostComponent
+    AddPostComponent,
+    ViewAllPostComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-all-post/view-all-post.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-post',
+  templateUrl: './view-all-post.component.html',
+  styleUrls: ['./view-all-post.component.css']
+})
+export class ViewAllPostComponent {
+
+}
