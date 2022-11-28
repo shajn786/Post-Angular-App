c7cd27f2b0098c1368c8bcb9e050c2bfb2c99121
shajn786 <shajn786@gmail.com>
2022-11-28 21:18:14 +0530

    reading values

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AddPostComponent {
 
+  userid=""
+  id=""
+  title=""
+  body=""
+
+  add =()=>
+  {
+    let data:any = {"userid":this.userid,"id":this.id,"title":this.title,"body":this.body}
+
+    console.log(data)
+  }
+
 }
